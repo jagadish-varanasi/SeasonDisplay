a3ba2d761463b04d48233cd9921f5c1bd9290b6e
vjagadishvaranasi <jagadishvaranasi123@gmail.com>
2020-05-04 20:02:50 +0530

    refactoring two ternary operations

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
+const sesonConfig={
+    summer:{
+        text:'Lets hit the beach!',
+        iconName:'sun'
+    },
+    winter:{
+        text:'Burr it is cold!',
+        iconName:'snowflak'
+    }
+}
+
 const getSeason=(lat,month)=>{
     if(month>2&&month<9){
         return lat>0?'summer':'winter';
@@ -13,15 +24,16 @@ const SeasonDisplay=(props)=>{
     
 
     const season=getSeason(props.lat,new Date().getMonth())
-    const text=season==='winter'?'Burr,it is chilly!':'Lets hit the beach';
-    //console.log(season);
-    // go to semantic ui--->icons-->ctrl+f search for snowflak and sun..see the source code and implement it
-    const icon=season==='winter'?'snowflak':'sun';
+    // const text=season==='winter'?'Burr,it is chilly!':'Lets hit the beach';
+    // //console.log(season);
+    // // go to semantic ui--->icons-->ctrl+f search for snowflak and sun..see the source code and implement it
+    // const icon=season==='winter'?'snowflak':'sun';
+    const {text,iconName}=sesonConfig[season]//retuns an object {text,iconName}
 return(
     <div>
-        <i className={`${icon} icon`}/>
+        <i className={`${iconName} icon`}/>
          <h1>{text}</h1>
-        <i className={`${icon} icon`}/>
+        <i className={`${iconName} icon`}/>
     </div>
 )
 }
